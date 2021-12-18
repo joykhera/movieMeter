b3ec717e1746c5ec8d9791d708a5b467a19abcb7
manticore999 <73572578+manticore999@users.noreply.github.com>
2021-12-18 14:31:02 -0500

    a

--- index.js
+++ index.js
@@ -8,15 +8,21 @@ function changeFunc() {
     getMovies(selectedValue);
 }
 
+function addMovies(movies) {
+    for (const movie of movies) {
+        const img = document.createElement("img")
+        img.setAttribute("src", `http://image.tmdb.org/t/p/w185${movie.poster_path}?api_key=44c162a820528a8e43d118f1e143586e`)
+        const a = document.createElement("a")
+        a.setAttribute("href", `${location.hostname == "joykhera.github.io" ? '/movieMeter' : ''}/rate?id=${movie.id}`)
+        a.append(img)
+        document.getElementById("movies").append(a)
+    }
+}
+
 async function getMovies(category) {
     document.getElementById('movies').innerHTML = ''
     const movies = await (await (await fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=44c162a820528a8e43d118f1e143586e`)).json()).results
-    for (const movie of movies) {
-        document.getElementById('movies').innerHTML += `<a
-            href="/rate?id=${movie.id}">
-            <img src="http://image.tmdb.org/t/p/w185${movie.poster_path}?api_key=44c162a820528a8e43d118f1e143586e">
-        </a>`
-    }
+    addMovies(movies)
     currentCategory = category
 }
 
@@ -24,12 +30,6 @@ document.addEventListener('scroll', async () => {
     if (document.documentElement.scrollHeight - document.documentElement.scrollTop <= document.documentElement.clientHeight + 500) {
         page++
         const movies = await (await (await fetch(`https://api.themoviedb.org/3/movie/${currentCategory}?api_key=44c162a820528a8e43d118f1e143586e&page=${page}`)).json()).results
-        for (const movie of movies) {
-            console.log(location.hostname == "joykhera.github.io")
-            document.getElementById('movies').innerHTML += `<a
-            href="${location.hostname == "joykhera.github.io" ? '/movieMeter' : ''}/rate?id=${movie.id}">
-            <img src="http://image.tmdb.org/t/p/w185${movie.poster_path}?api_key=44c162a820528a8e43d118f1e143586e">
-        </a>`
-        }
+        addMovies(movies)
     }
 })
